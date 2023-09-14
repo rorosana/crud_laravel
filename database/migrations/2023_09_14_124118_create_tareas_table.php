5640bb3f3dbb3c416a0359e349d604077050c023
rorosana <r.a.caraballo8@gmail.com>
2023-09-14 18:48:28 +0200

    MVC

---
+++ database/migrations/2023_09_14_124118_create_tareas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 60);
+            $table->string('descripcion')->nullable();//si no marco longitud me lo crea de 255 de largo.
+            $table->tinyInteger('finalizada')->default(0);
+            $table->timestamp('fecha limite');
+            $table->tinyInteger('urgencia')->comment('0: No es urgente, 1: Urgencia normal, 2: Muy urgente');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
